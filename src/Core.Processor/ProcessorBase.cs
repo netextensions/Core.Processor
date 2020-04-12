e9871d81fe89b67cf65e72aabb3d0731bbebcdd7
Csaba Salat <csalat@globalblue.com>
2020-04-12 13:33:55 +0200

    add ProcessorBase

---
+++ src/Core.Processor/ProcessorBase.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NetExtensions
+{
+    public abstract class ProcessorBase<T> : IProcessor<T>
+    {
+        private readonly IProcessor<T>[] _processors;
+
+        protected ProcessorBase(params IProcessor<T>[] processors)
+        {
+            _processors = processors;
+        }
+        public async Task<T> ExecuteAsync(T payload, CancellationToken token)
+        {
+            var result = _processors.Aggregate((IProcessor<T>)new NullProcessor<T>(), (current, processor) => ChainProcessorExtension.Then<T>(current, processor));
+            return await result.ExecuteAsync(payload, token).ConfigureAwait(false);
+        }
+    }
+}
